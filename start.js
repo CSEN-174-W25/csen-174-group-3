f42eccf33e9fc39581424a91d4aecb47a9f1ce92
Salvador Solana <2304664@ucc.edu.ar>
2025-02-02 14:25:35 -0800

    database extention plus, main goals adjustements

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const calculateBMI = async () => {
   const height = document.getElementById('height').value;
   const weight = document.getElementById('weight').value;
+  const goal = document.getElementById('goal').value;
 
   const bmi = weight / ((height / 100) * (height / 100));
 
@@ -15,7 +16,7 @@ const calculateBMI = async () => {
       result = 'Height cannot be greater than 251cm or less than 40cm.';
   } else {
       result = `Your BMI is: ${bmi.toFixed(2)}`;
-      await showPlan(bmi); // Call showPlan with the calculated BMI
+      await showPlan(bmi, goal); // Call showPlan with the calculated BMI and goal
   }
 
   document.getElementById('result').innerHTML = result;
@@ -25,27 +26,28 @@ document.getElementById('btn-show-plan').addEventListener("click", async () => {
   document.getElementById('result2').style.display = 'flex';
   const height = document.getElementById('height').value;
   const weight = document.getElementById('weight').value;
+  const goal = document.getElementById('goal').value;
   const bmi = weight / ((height / 100) * (height / 100));
-  await showPlan(bmi);
+  await showPlan(bmi, goal);
 });
 
-const showPlan = async (bmi) => {
+const showPlan = async (bmi, goal) => {
   let result2 = '';
   if (isNaN(bmi)) {
       result2 = 'Please enter valid numeric values.';
   } else {
       const workouts = await loadWorkouts();
       const filteredWorkouts = workouts.filter(workout => {
-          if (bmi < 18.5) return workout.type === 'strength';
-          if (bmi >= 18.5 && bmi < 25) return workout.type === 'cardio';
-          if (bmi >= 25 && bmi < 30) return workout.type === 'strength';
-          if (bmi >= 30) return workout.type === 'cardio';
+          if (goal === 'weight loss') return workout.type === 'cardio';
+          if (goal === 'muscle gain') return workout.type === 'strength';
+          if (goal === 'maintenance') return workout.type === 'cardio' || workout.type === 'strength';
       });
 
       result2 = filteredWorkouts.map(workout => `
         <div class="workout">
           <h3>${workout.name}</h3>
           <p>${workout.description}</p>
+          <p>Time: ${workout["time (minutes)"]} minutes</p>
         </div>
       `).join('');
   }
